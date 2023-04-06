165b38caa2376d4776f13d569ad5b631343f2ec7
chumpblocckami <mazzolauni@gmail.com>
2023-04-06 15:48:09 +0200

    feat: first commit

---
+++ build/lib/scrying/__init__.py
@@ -0,0 +1,9 @@
+"""
+Scrying.
+
+A ScryfallAPI port written in Python.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Matteo Mazzola'
+__credits__ = 'Vedalken'
---
+++ example.py
@@ -0,0 +1,4 @@
+from scrying.main import Scrying
+
+scry = Scrying()
+scry.download_from_url("f:pauper t:creature t:human c:white")
